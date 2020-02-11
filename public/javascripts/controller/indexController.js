455d0c5592199181afc6fd867fefa7d2debad7bf
ykemals <y.kemalsert@gmail.com>
2020-02-12 00:55:42 +0300

    users

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,14 +1,26 @@
 app.controller('indexController', ['$scope','indexFactory',($scope,indexFactory) => {
 
-    const connectionOptions={
-        reconnectionAttempts:3,
-        reconnectionDelay:600
-    };
+    $scope.init = () => {
+        const username = prompt('Please enter username');
 
-    indexFactory.connectSocket('http://localhost:3000',connectionOptions)
-        .then((socket)=>{
-            console.log('Bağlantı gerçekleşti',socket);
-          }).catch((err)=>{
-        console.log(err);
-    });
+        if(username)
+            initSocket(username);
+        else
+            return false;
+    };
+    
+    function initSocket(username) {
+        const connectionOptions={
+            reconnectionAttempts:3,
+            reconnectionDelay:600
+        };
+
+        indexFactory.connectSocket('http://localhost:3000',connectionOptions)
+            .then((socket)=>{
+                socket.emit('newUser',{username});
+                console.log('Bağlantı gerçekleşti',socket);
+            }).catch((err)=>{
+            console.log(err);
+        });
+    }
 }]);
